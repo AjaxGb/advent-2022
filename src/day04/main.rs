b8ebaab64b5398867a26d719ab3ce47f8248fc2a
Sol Toder <ajaxgb@gmail.com>
2022-12-04 00:38:12 -0500

    Day 4, Part 2 complete

--- src/day04/main.rs
+++ src/day04/main.rs
@@ -9,17 +9,26 @@ impl IdRange {
         let (min, max) = text.split_once('-').unwrap();
         let min = min.parse().unwrap();
         let max = max.parse().unwrap();
+        assert!(
+            min <= max,
+            "minimum ({min}) was greater than maximum ({max})"
+        );
         Self { min, max }
     }
-    
+
     fn contains(self, other: IdRange) -> bool {
         self.min <= other.min && self.max >= other.max
+    }
+
+    fn overlaps(self, other: IdRange) -> bool {
+        self.min <= other.max && self.max >= other.min
     }
 }
 
 fn main() {
     let mut fully_contains_count = 0;
-    
+    let mut overlaps_count = 0;
+
     for line in include_str!("input.txt").lines() {
         let (a, b) = line.split_once(',').unwrap();
         let a = IdRange::parse(a);
@@ -27,7 +36,11 @@ fn main() {
         if a.contains(b) || b.contains(a) {
             fully_contains_count += 1;
         }
+        if a.overlaps(b) {
+            overlaps_count += 1;
+        }
     }
-    
+
     println!("Pairs where one fully contains the other: {fully_contains_count}");
+    println!("Pairs where one overlaps the other: {overlaps_count}");
 }
