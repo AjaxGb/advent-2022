b5f68d2a06530e813ee70701d668cc22df4f8e9e
Sol Toder <ajaxgb@gmail.com>
2022-12-04 00:30:38 -0500

    Day 4, Part 1 complete

---
+++ src/day04/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct IdRange {
+    min: u32,
+    max: u32,
+}
+
+impl IdRange {
+    fn parse(text: &str) -> Self {
+        let (min, max) = text.split_once('-').unwrap();
+        let min = min.parse().unwrap();
+        let max = max.parse().unwrap();
+        Self { min, max }
+    }
+    
+    fn contains(self, other: IdRange) -> bool {
+        self.min <= other.min && self.max >= other.max
+    }
+}
+
+fn main() {
+    let mut fully_contains_count = 0;
+    
+    for line in include_str!("input.txt").lines() {
+        let (a, b) = line.split_once(',').unwrap();
+        let a = IdRange::parse(a);
+        let b = IdRange::parse(b);
+        if a.contains(b) || b.contains(a) {
+            fully_contains_count += 1;
+        }
+    }
+    
+    println!("Pairs where one fully contains the other: {fully_contains_count}");
+}
