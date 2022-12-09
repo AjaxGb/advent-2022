8bfa1f5694c163bf7b6a9548ac633715fa72afe1
Sol Toder <ajaxgb@gmail.com>
2022-12-09 02:43:06 -0500

    Day 9, Part 2 complete

--- src/day09/main.rs
+++ src/day09/main.rs
@@ -1,7 +1,5 @@
-use std::{
-    collections::HashSet,
-    ops::{Add, AddAssign, Sub},
-};
+use std::collections::HashSet;
+use std::ops::{AddAssign, Sub};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct RopeVec {
@@ -10,6 +8,8 @@ struct RopeVec {
 }
 
 impl RopeVec {
+    pub const ZERO: Self = Self::new(0, 0);
+
     pub const fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
@@ -29,14 +29,10 @@ impl RopeVec {
     }
 }
 
-impl Add for RopeVec {
-    type Output = RopeVec;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
+impl AddAssign for RopeVec {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
 }
 
@@ -51,19 +47,64 @@ impl Sub for RopeVec {
     }
 }
 
-impl AddAssign for RopeVec {
-    fn add_assign(&mut self, rhs: Self) {
-        self.x += rhs.x;
-        self.y += rhs.y;
+#[derive(Debug, Clone)]
+struct Rope<const N: usize> {
+    knots: [RopeVec; N],
+    tail_history: HashSet<RopeVec>,
+}
+
+impl<const N: usize> Rope<N> {
+    pub fn new() -> Self {
+        if N == 0 {
+            panic!("Rope must have at least one element");
+        }
+        let mut tail_history = HashSet::new();
+        tail_history.insert(RopeVec::ZERO);
+        Self {
+            knots: [RopeVec::ZERO; N],
+            tail_history,
+        }
+    }
+
+    pub const fn head(&self) -> RopeVec {
+        self.knots[0]
+    }
+
+    pub const fn tail(&self) -> RopeVec {
+        self.knots[N - 1]
+    }
+
+    pub const fn tail_history(&self) -> &HashSet<RopeVec> {
+        &self.tail_history
+    }
+
+    fn simulate_knot(target: RopeVec, knot: &mut RopeVec) -> bool {
+        let to_target = target - *knot;
+        let dist = to_target.abs();
+        let knot_moves = dist.y > 1 || dist.x > 1;
+        if knot_moves {
+            *knot += to_target.signum();
+        }
+        knot_moves
+    }
+
+    pub fn move_head(&mut self, offset: RopeVec) {
+        self.knots[0] += offset;
+
+        let mut target_pos = self.head();
+        for curr_pos in &mut self.knots[1..N - 1] {
+            Self::simulate_knot(target_pos, curr_pos);
+            target_pos = *curr_pos;
+        }
+        if Self::simulate_knot(target_pos, &mut self.knots[N - 1]) {
+            self.tail_history.insert(self.tail());
+        }
     }
 }
 
 fn main() {
-    let mut head_pos = RopeVec::new(0, 0);
-    let mut tail_pos = RopeVec::new(0, 0);
-
-    let mut all_tail_pos = HashSet::new();
-    all_tail_pos.insert(tail_pos);
+    let mut rope_p1 = Rope::<2>::new();
+    let mut rope_p2 = Rope::<10>::new();
 
     for line in include_str!("input.txt").lines() {
         let (dir, dist) = line.split_once(' ').unwrap();
@@ -77,16 +118,11 @@ fn main() {
         let dist: u32 = dist.parse().unwrap();
 
         for _ in 0..dist {
-            head_pos += offset;
-
-            let to_head = head_pos - tail_pos;
-            let dist = to_head.abs();
-            if dist.y > 1 || dist.x > 1 {
-                tail_pos += to_head.signum();
-                all_tail_pos.insert(tail_pos);
-            }
+            rope_p1.move_head(offset);
+            rope_p2.move_head(offset);
         }
     }
 
-    println!("Num tail positions: {}", all_tail_pos.len());
+    println!("Num tail positions (P1): {}", rope_p1.tail_history().len());
+    println!("Num tail positions (P2): {}", rope_p2.tail_history().len());
 }
