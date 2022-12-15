3938d8ba1d103489a3af0aa23915e255ed4c52a5
Sol Toder <ajaxgb@gmail.com>
2022-12-14 23:33:13 -0500

    Day 14, Part 2 complete

--- src/day14/main.rs
+++ src/day14/main.rs
@@ -1,114 +1,56 @@
-use std::{
-    io::{self, stdout},
-    num::ParseIntError,
-    str::FromStr,
-    time::Duration,
-};
+use std::{collections::HashSet, num::ParseIntError, str::FromStr};
 
 use advent_2022::Vec2;
 use thiserror::Error;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub enum Tile {
-    #[default]
-    Air,
-    Stone,
-    Sand,
-}
-
-impl Tile {
-    pub fn is_air(self) -> bool {
-        self == Self::Air
-    }
-}
-
+#[derive(Debug, Clone)]
 pub struct SandSim {
-    width: usize,
-    height: usize,
-    top_left: Vec2,
-    grid: Vec<Tile>,
+    grid: HashSet<Vec2>,
     falling_sand: Option<Vec2>,
     resting_sand: u32,
+    floor_y: i32,
+    has_floor: bool,
 }
 
 impl SandSim {
     pub const SPAWN_POS: Vec2 = Vec2::new(500, 0);
 
-    pub const fn width(&self) -> usize {
-        self.width
-    }
-
-    pub const fn height(&self) -> usize {
-        self.height
-    }
-
     pub const fn resting_sand(&self) -> u32 {
         self.resting_sand
     }
 
-    pub fn draw(&self, out: &mut impl io::Write) -> io::Result<()> {
-        let mut buffer = vec![b'\0'; self.width + 1];
-        buffer[self.width] = b'\n';
-        for row in self.grid.chunks_exact(self.width) {
-            for (i, tile) in row.iter().enumerate() {
-                buffer[i] = match *tile {
-                    Tile::Air => b'.',
-                    Tile::Stone => b'#',
-                    Tile::Sand => b'o',
-                }
-            }
-            out.write(&buffer)?;
-        }
-        Ok(())
-    }
-
-    fn pos_to_index(&self, pos: Vec2) -> Option<usize> {
-        let loc = pos - self.top_left;
-        if loc.x < 0 || loc.x as usize > self.width {
-            None
-        } else if loc.y < 0 || loc.y as usize > self.height {
-            None
-        } else {
-            Some(loc.x as usize + loc.y as usize * self.width)
-        }
-    }
-
-    pub fn get_mut(&mut self, pos: Vec2) -> Option<&mut Tile> {
-        self.pos_to_index(pos).map(|i| &mut self.grid[i])
-    }
-
-    pub fn get(&self, pos: Vec2) -> Option<&Tile> {
-        self.pos_to_index(pos).map(|i| &self.grid[i])
-    }
-
     pub fn update(&mut self) -> bool {
-        let Some(old_pos) = self.falling_sand else {
-            self.falling_sand = Some(Self::SPAWN_POS);
-            let tile = self.get_mut(Self::SPAWN_POS).unwrap();
-            *tile = Tile::Sand;
-            return false;
-        };
-
-        for dir in [Vec2::DOWN, Vec2::DOWN_LEFT, Vec2::DOWN_RIGHT] {
-            let new_pos = old_pos + dir;
-            if let Some(new_tile) = self.get_mut(new_pos) {
-                if new_tile.is_air() {
-                    *new_tile = Tile::Sand;
-                    *self.get_mut(old_pos).unwrap() = Tile::Air;
-                    self.falling_sand = Some(new_pos);
-                    return false;
+        if let Some(old_pos) = self.falling_sand {
+            if old_pos.y + 1 >= self.floor_y {
+                self.falling_sand = None;
+                if self.has_floor {
+                    self.resting_sand += 1;
+                    false
+                } else {
+                    self.grid.remove(&old_pos);
+                    true
                 }
             } else {
-                // It fell out of the sim space
-                *self.get_mut(old_pos).unwrap() = Tile::Air;
+                for dir in [Vec2::DOWN, Vec2::DOWN_LEFT, Vec2::DOWN_RIGHT] {
+                    let new_pos = old_pos + dir;
+                    if self.grid.insert(new_pos) {
+                        self.grid.remove(&old_pos);
+                        self.falling_sand = Some(new_pos);
+                        return false;
+                    }
+                }
                 self.falling_sand = None;
-                return true;
-            };
+                self.resting_sand += 1;
+                false
+            }
+        } else {
+            if self.grid.insert(Self::SPAWN_POS) {
+                self.falling_sand = Some(Self::SPAWN_POS);
+                false
+            } else {
+                true
+            }
         }
-
-        self.falling_sand = None;
-        self.resting_sand += 1;
-        false
     }
 }
 
@@ -127,10 +69,7 @@ impl FromStr for SandSim {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         use ParseSandSimError::*;
-        let mut line_defs = vec![];
-        let mut min_x = Self::SPAWN_POS.x;
-        let mut max_x = Self::SPAWN_POS.x;
-        let mut min_y = Self::SPAWN_POS.y;
+        let mut grid = HashSet::new();
         let mut max_y = Self::SPAWN_POS.y;
         for line in s.lines() {
             let mut points = line.split(" -> ").map(|s| {
@@ -139,9 +78,6 @@ impl FromStr for SandSim {
                 };
                 let x = x.parse()?;
                 let y = y.parse()?;
-                min_x = min_x.min(x);
-                max_x = max_x.max(x);
-                min_y = min_y.min(y);
                 max_y = max_y.max(y);
                 Ok(Vec2::new(x, y))
             });
@@ -151,56 +87,35 @@ impl FromStr for SandSim {
             let mut prev = start?;
             for point in points {
                 let point = point?;
-                line_defs.push(if prev.x == point.x {
-                    if prev.y <= point.y {
-                        (prev, point)
-                    } else {
-                        (point, prev)
-                    }
-                } else if prev.y == point.y {
-                    if prev.x <= point.x {
-                        (prev, point)
-                    } else {
-                        (point, prev)
-                    }
-                } else {
+                if prev.x != point.x && prev.y != point.y {
                     return Err(DiagonalLine(prev, point));
-                });
+                }
+                for x in prev.x.min(point.x)..=prev.x.max(point.x) {
+                    for y in prev.y.min(point.y)..=prev.y.max(point.y) {
+                        grid.insert(Vec2 { x, y });
+                    }
+                }
                 prev = point;
             }
         }
-        let width = (max_x - min_x) as usize + 1;
-        let height = (max_y - min_y) as usize + 1;
-        let mut grid = vec![Tile::Air; width * height];
-        let top_left = Vec2::new(min_x, min_y);
-        for (a, b) in line_defs {
-            for x in a.x..=b.x {
-                for y in a.y..=b.y {
-                    let point = Vec2::new(x, y) - top_left;
-                    let index = point.x as usize + point.y as usize * width;
-                    grid[index] = Tile::Stone;
-                }
-            }
-        }
         Ok(Self {
-            width,
-            height,
-            top_left,
             grid,
             falling_sand: None,
             resting_sand: 0,
+            floor_y: max_y + 2,
+            has_floor: false,
         })
     }
 }
 
 fn main() {
-    let mut out = stdout().lock();
-    let mut sim: SandSim = include_str!("input.txt").parse().unwrap();
-    sim.draw(&mut out).unwrap();
-    loop {
-        if sim.update() {
-            break;
-        }
-    }
-    println!("Resting sand: {}", sim.resting_sand());
+    let mut sim1: SandSim = include_str!("input.txt").parse().unwrap();
+    let mut sim2 = sim1.clone();
+    sim2.has_floor = true;
+    
+    while !sim1.update() {}
+    println!("Resting sand, Part 1: {}", sim1.resting_sand());
+    
+    while !sim2.update() {}
+    println!("Resting sand, Part 2: {}", sim2.resting_sand());
 }
